343538d1764413f2d5cdffc0d89243aecfc86395
Leticia-SS <leticia.saraiva013@gmail.com>
2024-12-13 21:40:52 -0300

    feat: ajust snack project

---
+++ movle-mobile-app/app.js
@@ -0,0 +1,11 @@
+import { Text, SafeAreaView, StyleSheet } from 'react-native';
+import HomeScreen from './app/(tabs)/index'
+
+
+export default function App() {
+  return (
+    <SafeAreaView >
+      <HomeScreen/>
+    </SafeAreaView>
+  );
+}
